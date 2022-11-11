232a62b359a6110683e4295a9edd078cfd3ae115
Teddy <Teddyleyva12@gmail.com>
2022-11-10 20:45:39 -0700

    implemented product

--- stage_3/Product.java
+++ stage_3/Product.java
@@ -12,4 +12,40 @@
 package stage_3;
 
 public class Product {
+    private String distributor;
+    private String productId;
+    private String productName;
+    private double unitPrice;
+
+    public String getDistributor() {
+        return distributor;
+    }
+
+    public void setDistributor(String distributor) {
+        this.distributor = distributor;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
