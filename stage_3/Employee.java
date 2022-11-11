c76906615c94e85785dcce77e1c3a831a5c2a012
Mathias Kana <mathias.kanasounna@enmu.edu>
2022-11-10 21:36:29 -0600

    Class Creation Update

--- stage_3/Employee.java
+++ stage_3/Employee.java
@@ -9,5 +9,52 @@
  * */
 package stage_3;
 
+import java.util.Date;
+
 public class Employee {
+    private String employeeId;
+    private Date hireDate;
+    private Date leaveDate;
+    private String employeeStatus;
+    private  String userName;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Date getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(Date leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public String getEmployeeStatus() {
+        return employeeStatus;
+    }
+
+    public void setEmployeeStatus(String employeeStatus) {
+        this.employeeStatus = employeeStatus;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
--- stage_3/Person.java
+++ stage_3/Person.java
@@ -10,4 +10,12 @@
 package stage_3;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String streetAddress;
+    private String city;
+    private String zipCode;
+    private String state;
+    private String telephone;
 }
