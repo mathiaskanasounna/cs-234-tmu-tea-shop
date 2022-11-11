6db1ee1fa35a843f2992e895b7ccfaee94b45817
Mathias Kana <mathias.kanasounna@enmu.edu>
2022-11-10 21:15:38 -0600

    Class Creation.

---
+++ stage_3/Product.java
@@ -0,0 +1,15 @@
+
+/*
+*
+ *   Name:
+ *      - Mathias K.
+ *      - Theodoro L.
+ *   Date: 11/13/2022
+ *   Title: Project Stage 3
+ *   Class: CS-234
+ *
+ * */
+package stage_3;
+
+public class Product {
+}
---
+++ stage_3/TeaShop.java
@@ -0,0 +1,13 @@
+/**
+ *   Name:
+ *      - Mathias K.
+ *      - Theodoro L.
+ *   Date: 11/13/2022
+ *   Title: Project Stage 3
+ *   Class: CS-234
+ *
+ * */
+package stage_3;
+
+public class TeaShop {
+}
