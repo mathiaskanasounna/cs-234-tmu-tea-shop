f5d36feed8beb3c722444e1ecc9fcf7bbf19b530
Teddy <Teddyleyva12@gmail.com>
2022-11-11 09:22:04 -0700

    implemented promotion class

--- stage_3/Promotion.java
+++ stage_3/Promotion.java
@@ -10,4 +10,39 @@
 package stage_3;
 
 public class Promotion {
+
+    private String promotionID;
+    private Date startDate;
+    private Date endDate;
+    private String promotionDescription;
+    private String couponCode;
+    private double discountRate;
+
+    public String getPromotionID() {
+        return promotionID;
+    }
+
+    public void setPromotionID(String promotionID) {
+        this.promotionID = promotionID;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    public String sendPromotion(){
+
+    }
+    public void createPromotion(String promotion ,double discount)
 }
--- stage_3/Sale.java
+++ stage_3/Sale.java
@@ -8,6 +8,39 @@
  *
  * */
 package stage_3;
+import java.util.ArrayList;
 
 public class Sale {
+    private String orderId;
+    private String orderDate;
+    private Arraylist<String> products;
+    public Sale(){
+        this.products = new ArrayList<>();
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+    public void getProducts(product){
+
+    }
+    public int totalPrice(){
+
+    }
+    public int numberOfItems(){
+        return products.size;
+    }
+
 }
